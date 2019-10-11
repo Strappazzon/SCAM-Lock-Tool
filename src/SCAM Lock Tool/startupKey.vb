3330987ca65e162373264503d02b088be18b8edf
Strappazzon <strappazzon@protonmail.com>
2019-10-11 03:32:16 +0200

    Version 1.1.0
    - Accept command line arguments: --no-wikipedia, --no-error and --success
    - Check both password fields
    - IsNot -> <>

--- src/SCAM Lock Tool/startupKey.vb
+++ src/SCAM Lock Tool/startupKey.vb
@@ -1,4 +1,6 @@
 ﻿Public Class startupKey
+    Public cmdArgs = Environment.GetCommandLineArgs()
+
     Private Sub startupKey_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Form1.Enabled = False
         passwStartupTextBox.Select()
@@ -16,19 +18,26 @@
     End Sub
 
     Private Sub okStartupBtn_Click(sender As Object, e As EventArgs) Handles okStartupBtn.Click
-        If passwStartupTextBox.Text = "" Then
+        If passwStartupTextBox.Text = "" Or passwStartupConfirmTextBox.Text = "" Then
             MessageBox.Show("The password cannot be empty.", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        ElseIf passwStartupTextBox.Text = "hunter2" And passwStartupConfirmTextBox.Text = "hunter2" Then
-            MessageBox.Show("The Account Database Startup Key was changed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            Application.Exit()
         ElseIf passwStartupTextBox.Text = passwStartupConfirmTextBox.Text Then
-            Dim result As Integer = MessageBox.Show("An error occurred while scamming this user with the password '" & passwStartupTextBox.Text & "'", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
-            If result = DialogResult.Retry Or result = DialogResult.Cancel Then
-                Threading.Thread.Sleep(1500)
+            Try
+                If cmdArgs(1) = "--no-wikipedia" Then
+                    MessageBox.Show("An error occurred while changing the Account Database Startup Key", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+                ElseIf cmdArgs(1) = "--no-error" Xor cmdArgs(1) = "--success" Then
+                    MessageBox.Show("The Account Database Startup Key was changed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    Application.Exit()
+                Else
+                    MessageBox.Show("An error occurred while scamming this user with the password '" & passwStartupTextBox.Text & "'", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+                    Process.Start("iexplore.exe", "https://en.wikipedia.org/wiki/Technical_support_scam")
+                    Application.Exit()
+                End If
+            Catch ex As IndexOutOfRangeException
+                MessageBox.Show("An error occurred while scamming this user with the password '" & passwStartupTextBox.Text & "'", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
                 Process.Start("iexplore.exe", "https://en.wikipedia.org/wiki/Technical_support_scam")
                 Application.Exit()
-            End If
-        ElseIf passwStartupTextBox.Text IsNot passwStartupConfirmTextBox.Text Then
+            End Try
+        ElseIf passwStartupTextBox.Text <> passwStartupConfirmTextBox.Text Then
             MessageBox.Show("The passwords entered do not match." & ControlChars.NewLine & "Please re-enter the passwords.", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             passwStartupTextBox.Clear()
             passwStartupConfirmTextBox.Clear()
