0190a0e09fcdbf83c7b052e8532713f18cf5d891
Strappazzon <strappazzon@protonmail.com>
2019-12-02 17:20:03 +0100

    Refactoring

--- src/SCAM Lock Tool/startupKey.vb
+++ src/SCAM Lock Tool/startupKey.vb
@@ -20,23 +20,21 @@
     Private Sub okStartupBtn_Click(sender As Object, e As EventArgs) Handles okStartupBtn.Click
         If passwStartupTextBox.Text = "" Or passwStartupConfirmTextBox.Text = "" Then
             MessageBox.Show("The password cannot be empty.", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        ElseIf passwStartupTextBox.Text = passwStartupConfirmTextBox.Text Then
-            Try
-                If cmdArgs(1) = "--no-wikipedia" Then
-                    MessageBox.Show("An error occurred while changing the Account Database Startup Key", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
-                ElseIf cmdArgs(1) = "--no-error" Xor cmdArgs(1) = "--success" Then
-                    MessageBox.Show("The Account Database Startup Key was changed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                    Application.Exit()
-                Else
-                    MessageBox.Show("An error occurred while scamming this user with the password '" & passwStartupTextBox.Text & "'", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
-                    Process.Start("iexplore.exe", "https://en.wikipedia.org/wiki/Technical_support_scam")
-                    Application.Exit()
-                End If
-            Catch ex As IndexOutOfRangeException
+        ElseIf passwStartupTextBox.Text = passwStartupConfirmTextBox.Text And My.Application.CommandLineArgs.Count > 0 Then
+            If cmdArgs(1) = "--no-wikipedia" Then
+                MessageBox.Show("An error occurred while changing the Account Database Startup Key", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+            ElseIf cmdArgs(1) = "--no-error" Xor cmdArgs(1) = "--success" Then
+                MessageBox.Show("The Account Database Startup Key was changed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Application.Exit()
+            Else
                 MessageBox.Show("An error occurred while scamming this user with the password '" & passwStartupTextBox.Text & "'", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
                 Process.Start("iexplore.exe", "https://en.wikipedia.org/wiki/Technical_support_scam")
                 Application.Exit()
-            End Try
+            End If
+        ElseIf passwStartupTextBox.Text = passwStartupConfirmTextBox.Text And My.Application.CommandLineArgs.Count = 0 Then
+            MessageBox.Show("An error occurred while scamming this user with the password '" & passwStartupTextBox.Text & "'", "System Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+            Process.Start("iexplore.exe", "https://en.wikipedia.org/wiki/Technical_support_scam")
+            Application.Exit()
         ElseIf passwStartupTextBox.Text <> passwStartupConfirmTextBox.Text Then
             MessageBox.Show("The passwords entered do not match." & ControlChars.NewLine & "Please re-enter the passwords.", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             passwStartupTextBox.Clear()
